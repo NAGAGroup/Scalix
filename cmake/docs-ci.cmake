654059968b914567141cde299ee368224c2bf2a8
Jack Myers <jackhmyers97@gmail.com>
2023-12-11 12:23:05 -0800

    switched to doxygen-awesome-css for docs styling

--- cmake/docs-ci.cmake
+++ cmake/docs-ci.cmake
@@ -10,28 +10,6 @@ set(src "${PROJECT_SOURCE_DIR}")
 
 # ---- Dependencies ----
 
-set(mcss_SOURCE_DIR "${bin}/docs/.ci")
-if(NOT IS_DIRECTORY "${mcss_SOURCE_DIR}")
-  file(MAKE_DIRECTORY "${mcss_SOURCE_DIR}")
-  file(
-    DOWNLOAD
-    https://github.com/friendlyanon/m.css/releases/download/release-1/mcss.zip
-    "${mcss_SOURCE_DIR}/mcss.zip"
-    STATUS status
-    EXPECTED_MD5 00cd2757ebafb9bcba7f5d399b3bec7f)
-  if(NOT status MATCHES "^0;")
-    message(FATAL_ERROR "Download failed with ${status}")
-  endif()
-  execute_process(
-    COMMAND "${CMAKE_COMMAND}" -E tar xf mcss.zip
-    WORKING_DIRECTORY "${mcss_SOURCE_DIR}"
-    RESULT_VARIABLE result)
-  if(NOT result EQUAL "0")
-    message(FATAL_ERROR "Extraction failed with ${result}")
-  endif()
-  file(REMOVE "${mcss_SOURCE_DIR}/mcss.zip")
-endif()
-
 find_program(Python3_EXECUTABLE NAMES python3 python)
 if(NOT Python3_EXECUTABLE)
   message(FATAL_ERROR "Python executable was not found")
@@ -102,17 +80,14 @@ if(NOT DEFINED DOXYGEN_OUTPUT_DIRECTORY)
 endif()
 set(out "${DOXYGEN_OUTPUT_DIRECTORY}")
 
-foreach(file IN ITEMS Doxyfile conf.py)
+foreach(file IN ITEMS Doxyfile )
   configure_file("${src}/docs/${file}.in" "${bin}/docs/${file}" @ONLY)
 endforeach()
-
-set(mcss_script "${mcss_SOURCE_DIR}/documentation/doxygen.py")
-set(config "${bin}/docs/conf.py")
 
 file(REMOVE_RECURSE "${out}/html" "${out}/xml")
 
 execute_process(
-  COMMAND "${Python3_EXECUTABLE}" "${mcss_script}" "${config}"
+  COMMAND "doxygen" "${bin}/docs/Doxyfile"
   WORKING_DIRECTORY "${bin}/docs"
   RESULT_VARIABLE result)
 if(NOT result EQUAL "0")
--- cmake/docs.cmake
+++ cmake/docs.cmake
@@ -5,16 +5,6 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.24")
   set(extract_timestamps DOWNLOAD_EXTRACT_TIMESTAMP YES)
 endif()
 
-include(FetchContent)
-FetchContent_Declare(
-  mcss
-  URL https://github.com/friendlyanon/m.css/releases/download/release-1/mcss.zip
-  URL_MD5 00cd2757ebafb9bcba7f5d399b3bec7f
-  SOURCE_DIR "${PROJECT_BINARY_DIR}/mcss"
-  UPDATE_DISCONNECTED YES
-  ${extract_timestamps})
-FetchContent_MakeAvailable(mcss)
-
 find_package(Python3 3.6 REQUIRED)
 
 # ---- Declare documentation target ----
@@ -25,18 +15,15 @@ set(DOXYGEN_OUTPUT_DIRECTORY
 
 set(working_dir "${PROJECT_BINARY_DIR}/docs")
 
-foreach(file IN ITEMS Doxyfile conf.py)
+foreach(file IN ITEMS Doxyfile)
   configure_file("docs/${file}.in" "${working_dir}/${file}" @ONLY)
 endforeach()
-
-set(mcss_script "${mcss_SOURCE_DIR}/documentation/doxygen.py")
-set(config "${working_dir}/conf.py")
 
 add_custom_target(
   docs
   COMMAND "${CMAKE_COMMAND}" -E remove_directory
           "${DOXYGEN_OUTPUT_DIRECTORY}/html" "${DOXYGEN_OUTPUT_DIRECTORY}/xml"
-  COMMAND "${Python3_EXECUTABLE}" "${mcss_script}" "${config}"
-  COMMENT "Building documentation using Doxygen and m.css"
+  COMMAND "doxygen" "${working_dir}/Doxyfile"
+  COMMENT "Building documentation using Doxygen and awesome-doxygen"
   WORKING_DIRECTORY "${working_dir}"
   VERBATIM)
